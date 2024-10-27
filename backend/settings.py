979fb961c65149b44b1bcb3f1fac70546f29d40e
justjoseph0 <iamonowu@gmail.com>
2024-10-27 03:29:38 -0700

    Set STATIC_ROOT for static file collection

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -140,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
